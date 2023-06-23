29b0b854e2566ee1aef0c18c9c89a3d77b48d981
David Falson <david@falson.net>
2023-06-23 09:26:48 -0700

    update import to match variable correct variable names

--- jobly/src/jobs/JobCard.tsx
+++ jobly/src/jobs/JobCard.tsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Job } from '../types/types'
 
-export default function JobCard({id, title, salary, equity, company_handle}:Job) {
+export default function JobCard({id, title, salary, equity, companyHandle}:Job) {
     return (
         <div className="JobCard card"> 
         <div className="card-body">
           <h6 className="card-title">{title}</h6>
-          <p>{company_handle}</p>
+          <p>{companyHandle}</p>
           {salary && <div><small>Salary: {'$'+salary.toLocaleString()}</small></div>}
           {equity !== undefined && <div><small>Equity: {equity}</small></div>}
-
         </div>
       </div>
     )
---
+++ jobly/src/jobs/JobList.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { Job } from '../types/types';
+import JoblyApi from '../api'
+import JobCard from './JobCard';
+
+export default function JobList() {
+    const [jobs, setJobs] = useState<Job[]>();
+    const [title, setTitle] = useState<string>();
+    useEffect(() => {
+        async function getJobsOnMountAndSearch() {
+            setJobs(await JoblyApi.getJobs(title));
+            
+        }
+        getJobsOnMountAndSearch()
+    }, [title]);
+
+    if (!jobs) return <div>Loading...</div>
+
+    return (
+        <div className='col-md-8 offset-md-2 text-start'>
+            {jobs.map(
+                job =>
+                    <JobCard
+                        id={job.id}
+                        title={job.title}
+                        salary={job.salary}
+                        equity={job.equity}
+                        companyHandle={job.companyHandle}
+                    />
+            )}
+        </div>
+    )
+
+}
+
