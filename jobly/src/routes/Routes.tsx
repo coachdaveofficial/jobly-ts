28d1f72a99f632f05b0807a60ba662f8481981b6
David Falson <david@falson.net>
2023-06-23 09:35:13 -0700

    add PrivateRoute component to specific routes

--- jobly/src/routes/Routes.tsx
+++ jobly/src/routes/Routes.tsx
@@ -2,35 +2,59 @@ import React from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 import CompanyList from '../companies/CompanyList';
 import CompanyDetail from '../companies/CompanyDetail';
+import JobList from '../jobs/JobList';
+import Homepage from '../homepage/Homepage';
+import LoginForm from '../auth/LoginForm';
+import { AuthFuncProp } from '../types/types';
+import {PrivateRoute} from './PrivateRoute';
+import SignupForm from '../auth/SignupForm';
 
 
-export default function RoutesFunc() {
+
+
+export default function RoutesFunc({ login, signup, logout }: AuthFuncProp) {
     return (
         <Routes>
 
 
-            <Route path='/' element={<div>home</div>} />
+            <Route path='/' element={<Homepage />} />
+
+            <Route path='/login' element={<LoginForm login={login} />} />
 
 
-            <Route path='/companies' element={<CompanyList />} />
+            <Route path='/signup' element={<SignupForm signup={signup} />} />
 
 
+            {/* Private Routes */}
+            <Route
+                path="/companies"
+                element={
+                    <PrivateRoute path='/companies' component={CompanyList} />
+                }
+            />
 
+            <Route
+                path="/companies:handle"
+                element={
+                    <PrivateRoute path='/companies' component={CompanyDetail} />
+                }
+            />
 
-            <Route path='/companies/:handle' element={<CompanyDetail />} />
+            <Route
+                path="/jobs"
+                element={
+                    <PrivateRoute path='/jobs' component={JobList} />
+                }
+            />
 
+            {/* <Route
+                path="/profile"
+                element={
+                    <PrivateRoute path='/profile' component={} />
+                }
+            /> */}
 
-            <Route path='/jobs' element={<div>jobs</div>} />
-
-
-            <Route path='/login' element={<div>login</div>} />
-
-
-            <Route path='/signup' element={<div>signup</div>} />
-
-
-            <Route path='/profile' element={<div>profile</div>} />
-
+            {/* End of Private Routes */}
 
             <Route path='*' element={<Navigate to='/' />} />
 
