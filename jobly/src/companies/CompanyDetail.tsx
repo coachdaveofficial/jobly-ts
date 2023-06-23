64eb42c29368b0084fe12a69a4ff9875f148cf77
David Falson <david@falson.net>
2023-06-23 09:24:52 -0700

    show JobCards for every job in a company

--- jobly/src/companies/CompanyDetail.tsx
+++ jobly/src/companies/CompanyDetail.tsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import JoblyApi from "../api";
-import { Company, Job } from '../types/types';
+import { Company } from '../types/types';
+import JobCard from "../jobs/JobCard";
 
 
 export default function CompanyDetail() {
-    const {handle} = useParams();
+    const { handle } = useParams();
     const [company, setCompany] = useState<Company>();
-    const [jobs, setJobs] = useState<Job[]>([]);
 
     useEffect(function getCompanyAndJobsForUser() {
         async function getCompany() {
-          setCompany(await JoblyApi.getCompany(handle));    
+            setCompany(await JoblyApi.getCompany(handle));
         }
-    
+
         getCompany();
-      }, [handle]);
+    }, [handle]);
 
     if (!company) return <div>Loading...</div>
 
@@ -23,6 +23,16 @@ export default function CompanyDetail() {
         <div className="col-md-8 offset-md-2">
             <h4>{company.name}</h4>
             <p>{company.description}</p>
+            {company.jobs?.map(
+                job =>
+                    <JobCard
+                        id={job.id}
+                        title={job.title}
+                        salary={job.salary}
+                        equity={job.equity}
+                        companyHandle={job.companyHandle}
+                    />
+            )}
         </div>
     )
 }
