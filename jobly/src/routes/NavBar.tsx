19c9fde55b8e02ab7e9ff6aaac4eb21b2cf50107
David Falson <david@falson.net>
2023-06-25 16:56:44 -0700

    add ternary to change which links are shown if user is logged in

--- jobly/src/routes/NavBar.tsx
+++ jobly/src/routes/NavBar.tsx
@@ -1,37 +1,64 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from "react-router-dom";
 import { Navbar, Nav, NavItem } from "reactstrap";
+import UserContext from "../context/UserContext";
+import { LogoutFunction, NavBarProps } from '../types/types';
 
 
-export default function NavBarFunc() {
+export default function NavBarFunc({logout}:NavBarProps) {
+    const {currentUser} = useContext(UserContext);
     return (
-        <div style={{backgroundColor: "lightblue"}}>
+
+        <div style={{ backgroundColor: "lightblue" }}>
             <Navbar expand="md">
                 <NavLink to="/" className="navbar-brand">
                     Jobly
                 </NavLink>
 
                 <Nav className="ml-auto" navbar>
-                    <NavItem>
-                        <NavLink to="/companies" className="nav-link">
-                            Companies
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to="/jobs" className="nav-link">
-                            Jobs
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to="/profile" className="nav-link">
-                            Profile
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to="/login" className="nav-link">
-                            Login
-                        </NavLink>
-                    </NavItem>
+
+                    {
+                        currentUser ?
+                            <>
+                                <NavItem>
+                                    <NavLink to="/companies" className="nav-link">
+                                        Companies
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/jobs" className="nav-link">
+                                        Jobs
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/profile" className="nav-link">
+                                        Profile
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/logout" className="nav-link" onClick={() => logout()}>
+                                        Logout {currentUser.username}
+                                    </NavLink>
+                                </NavItem>
+                            </>
+                            :
+                            <>
+                                <NavItem>
+                                    <NavLink to="/login" className="nav-link">
+                                        Login
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink to="/signup" className="nav-link">
+                                        Signup
+                                    </NavLink>
+                                </NavItem>
+                            </>
+                    }
+
+
+
+
                 </Nav>
             </Navbar>
         </div>
