1c57b1e2d687048b1af618530a2004e5aa6dc07f
David Falson <david@falson.net>
2023-06-29 15:28:49 -0700

    add Homepage component and supply background image and styles

--- jobly/src/homepage/Homepage.tsx
+++ jobly/src/homepage/Homepage.tsx
@@ -1,11 +1,11 @@
-import React, {useContext} from 'react'
-
-
+import React from 'react';
+import './Homepage.css'; 
 
 export default function Homepage() {
-    return (
-        <div>
-            hello homepage
-        </div>
-    )
+  return (
+    <div className="homepage">
+      <h1 className="homepage-title">Jobly</h1>
+      <p className="homepage-subtitle">A job board app</p>
+    </div>
+  );
 }
