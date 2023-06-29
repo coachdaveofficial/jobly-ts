36bbc252441b33756831c88048545a1ffa907f1b
David Falson <david@falson.net>
2023-06-29 15:25:45 -0700

    add search function to sort jobs and function to keep track of job applications

--- jobly/src/jobs/JobCard.tsx
+++ jobly/src/jobs/JobCard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Job } from '../types/types'
 import UserContext from '../context/UserContext'
 import JoblyApi from '../api';
@@ -6,15 +6,25 @@ import JobAppsContext from '../context/JobAppsContext';
 
 
 export default function JobCard({ id, title, salary, equity, companyHandle }: Job) {
-  const {currentUser} = useContext(UserContext);
-  const {appliedJobsIds, setAppliedJobsIds} = useContext(JobAppsContext)
+  const { currentUser } = useContext(UserContext);
+  const { applicationIds, setApplicationIds } = useContext(JobAppsContext);
+  const [applied, setApplied] = useState<boolean>();
+
+
+  function hasAppliedToJob(id: number) {
+    return applicationIds.has(id);
+  }
+
+  useEffect(function updateAppliedStatus() {
+    setApplied(hasAppliedToJob(id));
+  }, [id, hasAppliedToJob]);
+
   async function handleApply() {
+    if (hasAppliedToJob(id)) return;
     try {
       await JoblyApi.applyToJob(currentUser?.username, id)
 
-
-
-      setAppliedJobsIds((prevData: Set<number>) => {
+      setApplicationIds((prevData: Set<number>) => {
         console.log(prevData);
         if (prevData !== null) {
           return new Set([...prevData, id]);
@@ -24,7 +34,7 @@ export default function JobCard({ id, title, salary, equity, companyHandle }: Jo
 
       return { success: true }
     } catch (error) {
-      console.error("signup failed", error)
+      console.error("Application failed", error)
       return { success: false, error }
     }
   }
@@ -35,17 +45,14 @@ export default function JobCard({ id, title, salary, equity, companyHandle }: Jo
         <p>{companyHandle}</p>
         {salary && <div><small>Salary: {'$' + salary.toLocaleString()}</small></div>}
         {equity !== undefined && <div><small>Equity: {equity}</small></div>}
-
-        {
-          appliedJobsIds?.find(appliedJobId => appliedJobId = id)
-            ?
-            <button className='float-end btn btn-primary' disabled>Applied</button>
-            :
-            <button className='float-end btn btn-primary' onClick={handleApply} >Apply</button>
-        }
-
+        <button
+              className="btn btn-info font-weight-bold text-uppercase float-end"
+              onClick={handleApply}
+              disabled={applied}
+          >
+            {applied ? "Applied" : "Apply"}
+          </button>
+        </div>
       </div>
-
-    </div>
-  )
+  );
 }
--- jobly/src/jobs/JobList.tsx
+++ jobly/src/jobs/JobList.tsx
@@ -2,22 +2,26 @@ import React, { useState, useEffect } from 'react'
 import { Job } from '../types/types';
 import JoblyApi from '../api'
 import JobCard from './JobCard';
+import SearchForm from '../common/SearchForm';
 
 export default function JobList() {
     const [jobs, setJobs] = useState<Job[]>();
-    const [title, setTitle] = useState<string>();
-    useEffect(() => {
-        async function getJobsOnMountAndSearch() {
-            setJobs(await JoblyApi.getJobs(title));
-            
-        }
-        getJobsOnMountAndSearch()
-    }, [title]);
+
+    useEffect(function getJobsOnMount() {
+        search()
+    }, []);
+
+    /** Triggered by search form submit; reloads jobs. */
+    async function search(title?: string) {
+        let jobs = await JoblyApi.getJobs(title);
+        setJobs(jobs);
+    }
 
     if (!jobs) return <div>Loading...</div>
 
     return (
         <div className='col-md-8 offset-md-2 text-start'>
+            <SearchForm searchFor={search} />
             {jobs.map(
                 job =>
                     <JobCard
