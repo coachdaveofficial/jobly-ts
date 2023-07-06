67bd6f3572c74dfdad557e3132035d94223552c2
David Falson <david@falson.net>
2023-07-05 19:06:59 -0700

    have search form auto search after 500 milliseconds

--- jobly/src/common/SearchForm.tsx
+++ jobly/src/common/SearchForm.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+
 import "./SearchForm.css";
 
 
@@ -15,42 +17,39 @@ import "./SearchForm.css";
  */
 
 interface SearchFormProps {
-    searchFor: (searchTerm: string | undefined) => void;
-  }
+  searchFor: (searchTerm: string | undefined) => void;
+}
 
 function SearchForm({ searchFor }: SearchFormProps) {
   console.debug("SearchForm", "searchFor=", typeof searchFor);
 
   const [searchTerm, setSearchTerm] = useState("");
-
-  /** Tell parent to filter */
-  function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
-    // take care of accidentally trying to search for just spaces
-    evt.preventDefault();
-    searchFor(searchTerm.trim() || undefined);
-    setSearchTerm(searchTerm.trim());
-  }
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 500);
 
   /** Update form fields */
   function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
     setSearchTerm(evt.target.value);
   }
 
+  // Use the debounced value and automatically search after 500 milliseconds
+  useEffect(() => {
+    
+    searchFor(debouncedSearchTerm.trim() || undefined);
+    // eslint-disable-next-line
+  }, [debouncedSearchTerm])
+
+
   return (
-      <div className="SearchForm mb-4">
-        <form className="Search form-inline" onSubmit={handleSubmit}>
-          <input
-              className="form-control form-control-lg flex-grow-1"
-              name="searchTerm"
-              placeholder="Enter search term..."
-              value={searchTerm}
-              onChange={handleChange}
-          />
-          <button type="submit" className="btn btn-lg btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
+    <div className="SearchForm my-4">
+
+        <input
+          className="form-control form-control-lg flex-grow-1"
+          name="searchTerm"
+          placeholder="Enter search term..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+    </div>
   );
 }
 
